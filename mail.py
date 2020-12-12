7675c10f15ab606188a0212fda18156ce16ef27e
JoepDev <66672754+JoepTM@users.noreply.github.com>
2020-12-12 19:36:22 +0100

    Update mail.py

--- mail.py
+++ mail.py
@@ -15,52 +15,61 @@ print ("""
 |__/     |__/ \_______/|__/|__/
 """)
 
-sender_email = "your-mail@gmail.com"
-receiver_email = "receiver-mail@gmail.com"
-password = getpass.getpass("Type your password and press enter:")
+sender_email = "bobusessocks@gmail.com"
+receiver_email = "joepvermue@gmail.com"
+password = getpass.getpass("Type your password and press enter: ")
+subject = input("Type the subject of the mail: ")
 
 message = MIMEMultipart("alternative")
-message["Subject"] = "multipart test"
+message["Subject"] = subject
 message["From"] = sender_email
 message["To"] = receiver_email
 
-# Create the plain-text and HTML version of your message
-text = """\
-HI,
-This is a test mail send with python.
-"""
-html = """\
-<html>
-  <body>
-    <p>Hi,<br>
-       This is a test mail send with python.<br>
-    </p>
-  </body>
-</html>
-"""
+def typeText():
+    print("Type the mail you want to send: ")
+    lines = []
+    while True:
+        line = input()
+        if line:
+            lines.append(line)
+        else:
+            break
+    mailText = '\n'.join(lines)
+       
+    if len(mailText) == 0:
+        print("You cant leave the message blank.")
+        typeText()
+    else:
+        print(mailText)
+        yn = input("\nThis is your mail  right now, do you want to send it like this? y/n: ")
 
-# Turn these into plain/html MIMEText objects
-part1 = MIMEText(text, "plain")
-part2 = MIMEText(html, "html")
+        if yn == "y":
 
-# Add HTML/plain-text parts to MIMEMultipart message
-# The email client will try to render the last part first
-message.attach(part1)
-message.attach(part2)
-
-# Create secure connection with server and send email
-context = ssl.create_default_context()
-
-try:
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(
-            sender_email, receiver_email, message.as_string()
-        )
-except smtplib.SMTPAuthenticationError:
-    print ("""\n
- /#### /## /####  /######                                         
-| ##_/| ##|_  ## /##__  ##                                        
+            try:
+                # Attach the typed stuff to the mail.
+                part1 = MIMEText(mailText, "plain")
+                message.attach(part1)
+                # Create secure connection with server and send email
+                context = ssl.create_default_context()
+                with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+                    server.login(sender_email, password)
+                    server.sendmail(
+                        sender_email, receiver_email, message.as_string()
+                    )
+                print("""
+  /$$$$$$                                                             
+ /$$__  $$                                                            
+| $$  \__/ /$$   /$$  /$$$$$$$  /$$$$$$$  /$$$$$$   /$$$$$$$  /$$$$$$$
+|  $$$$$$ | $$  | $$ /$$_____/ /$$_____/ /$$__  $$ /$$_____/ /$$_____/
+ \____  $$| $$  | $$| $$      | $$      | $$$$$$$$|  $$$$$$ |  $$$$$$ 
+ /$$  \ $$| $$  | $$| $$      | $$      | $$_____/ \____  $$ \____  $$
+|  $$$$$$/|  $$$$$$/|  $$$$$$$|  $$$$$$$|  $$$$$$$ /$$$$$$$/ /$$$$$$$/
+ \______/  \______/  \_______/ \_______/ \_______/|_______/ |_______/
+Succesfully send the E-Mail to""", receiver_email)
+            except smtplib.SMTPAuthenticationError:
+                print("""
+ /#### /## /####  /######     
+| ##_/| ##|_  ## /##__  ##    
 | ##  | ##  | ##| ##  \ ##  /#######  /#######  /######   /#######
 | ##  | ##  | ##| ######## /##_____/ /##_____/ /##__  ## /##_____/
 | ##  |__/  | ##| ##__  ##| ##      | ##      | ########|  ###### 
@@ -68,4 +77,7 @@ except smtplib.SMTPAuthenticationError:
 | #### /## /####| ##  | ##|  #######|  #######|  ####### /#######/
 |____/|__/|____/|__/  |__/ \_______/ \_______/ \_______/|_______/  
 Allow access to less secure apps on your gmail account. https://www.google.com/settings/security/lesssecureapps
-            """)
+""")
+        elif yn == "n":
+            typeText()
+typeText()
